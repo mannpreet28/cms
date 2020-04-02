26eeac60e4ed7c0792999d7f561b5dc69cea2e1a
Manpreet Kaur <preetm823@gmail.com>
2020-04-02 18:28:17 -0500

    Setip

--- wp-content/themes/oculistwebsite/template-parts/categories/features.php
+++ wp-content/themes/oculistwebsite/template-parts/categories/features.php
@@ -1,1 +1,47 @@
-<?php
+<section id="features-section" class="main-section blue" >
+	<h1 class="section-title">Features</h1>
+	<section class="sub-section" >
+
+		<!-- ARTICLE FOR BOOK AN APPOINTMENT  -->
+		<article>
+			<div class="icons">
+				<div class="icon-wrap">
+					<img src="images/book.png" alt="bookicon">
+				</div>
+				<div class="icon-text">
+					<h4>Book an appoinment</h4>
+					<p class="p-text">One can book an appointment accoring to their schedule.</p>
+				</div>
+			</div>
+		</article>
+
+
+		<!-- ARTICLE FOR SHOP  -->
+		<article>
+			<div class="icons">
+				<div class="icon-wrap">
+					<img src="images/shop.png" alt="bookicon">
+				</div>
+				<div class="icon-text">
+					<h4>Shop</h4>
+					<p class="p-text">People can shop perscription and sunglasses easily.</p>
+				</div>
+			</div>
+		</article>
+
+
+		<!-- ARTICLE FOR DOCTORS  -->
+		<article>
+			<div class="icons">
+				<div class="icon-wrap">
+					<img src="images/doc.png" alt="bookicon">
+				</div>
+				<div class="icon-text">
+					<h4>Doctor's Profile</h4>
+					<p class="p-text">They can check the doctor's profile.</p>
+				</div>
+			</div>
+		</article>
+
+	</section>
+</section>

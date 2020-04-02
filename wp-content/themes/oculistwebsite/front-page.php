850c9ea4cfdc5fbfb353865cf551c4461c0068de
Manpreet Kaur <preetm823@gmail.com>
2020-04-02 17:48:50 -0500

    Setip

---
+++ wp-content/themes/oculistwebsite/front-page.php
@@ -0,0 +1,16 @@
+<?php
+get_header();
+
+
+get_template_part('template-parts/categories/','top');
+get_template_part('template-parts/categories/','features');
+get_template_part('template-parts/categories/','shop');
+get_template_part('template-parts/categories/','doctor');
+get_template_part('template-parts/categories/','top');
+get_template_part('template-parts/categories/','members');
+
+
+
+get_footer();
+
+?>
---
+++ wp-content/themes/oculistwebsite/template-parts/categories/shop.php
@@ -0,0 +1,20 @@
+<section id="top-section" class="main-section white" >
+	<h1 class="section-title">Welcome to Oculist</h1>
+	<section class="sub-section" >
+
+
+
+
+		<article <?php post_class(); ?> >
+			<div class="img-wrapper">
+				<img src="<?php ?>" alt="clinic">
+			</div>
+		</article>
+
+
+
+
+
+	</section>
+</section>
+
